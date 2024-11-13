ebc36acc3aa406a1aeacc55a62544bde616e07b8
Benoit62 <benoit.wattinne@student.junia.com>
2024-11-13 13:36:44 +0100

    Config

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Pharmat
 Application pour la révision des QCMs de l'internat de pharmacie 
+
+## Utilisation
+
+Clonez le dépôt
+Installez toutes les dépendances avec `npm install`.
+
+Assurez-vous d'avoir une base de donnée MySQL sur le service de votre choix.
+Importez les bases de donnée `pharmat_db.sql` et `pharmat_session.sql`.
+Dans le fichier `.env`, modifiez les 8 éléments correspondant à la base de donnée.
+
+Assurez-vous d'avoir un serveur mail.
+Modifierz le fichier `.env` avec les identifiants du service mail.
---
+++ pharmat_session.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql-pharmat.alwaysdata.net
+-- Generation Time: Nov 13, 2024 at 01:35 PM
+-- Server version: 10.6.18-MariaDB
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pharmat_session`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `session_id` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `expires` int(11) UNSIGNED NOT NULL,
+  `data` mediumtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sessions`
+--
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`session_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
